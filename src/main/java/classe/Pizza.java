7f9d2d2eae8ecad8acb2c5c24cffdf67fbd99f19
anisskiller <anissaallou36@gmail.com>
2018-11-20 12:11:45 +0100

    Update Pizza.java

--- src/main/java/classe/Pizza.java
+++ src/main/java/classe/Pizza.java
@@ -1,137 +1,93 @@
 package classe;
 
-	public class Pizza {
-		/*Id (ou identifiant unique pour chaque Pizza) : 1
-			Code : MAR
-			Désignation : MARGARITA
-			Prix : 9,20 €
-		*/
-		private int idPizza;
-		private String code = null;
-		private String designation = null;
-		private Double price = null;
-		static int nbr =0;
-		
-		
-		/**
-		 * 
-		 * @param code de la pizza (les trois premieres lettre en Maj)
-		 * @param designation Nom de la pizza
-		 * @param price prix de la pizza
-		 */
-		public Pizza(String code, String designation, Double price) {
-			this.idPizza = nbr++;
-			this.code = code;
-			this.designation = designation;
-			this.price = price;
-		}
-		/**
-		 * 
-		 * @return retourne le code de la pizza
-		 */
-		public String getCode() {
-			return code;
-		}
-		/**
-		 * 
-		 * @param code modifi le code de la pizza
-		 */
-		public void setCode(String code) {
-			this.code = code;
-		}
-		/**
-		 * 
-		 * @return retourne la designation
-		 */
-		public String getDesignation() {
-			return designation;
-		}
-		/**
-		 * 
-		 * @param designation modifi la designation
-		 */
-		public void setDesignation(String designation) {
-			this.designation = designation;
-		}
-		/**
-		 * 
-		 * @return retourne le prix de la pizza
-		 */
-		public Double getPrice() {
-			return price;
-		}
-		/**
-		 * 
-		 * @param price modification du prix
-		 */
-		public void setPrice(Double price) {
-			this.price = price;
-		}
-		public int getIdPizza() {
-			return idPizza;
-		}
-		/**
-		 * 
-		 * @return methode pour afficher le menu de l'application
-		 */
-		
-		public static String displayMenu() {
-			return "***** Pizzeria Administration *****\r\n" + 
-					"1. Lister les pizzas\r\n" + 
-					"2. Ajouter une nouvelle pizza\r\n" + 
-					"3. Mettre à jour une pizza\r\n" + 
-					"4. Supprimer une pizza\r\n" + 
-					"99. Sortir";
-		}
-		public String toString() {
+public class Pizza {
+
+	public int id;
+	public String code;
+	public String designation;
+	public double prix;
 	
-			return this.code +" -> "+ this.designation +"("+ this.price+" €)";
-		}
+	private static  int count = 0;
+	
+	/**
+	 * @param id : identifiant de la pizza
+	 * @param code : code de la pizza
+	 * @param designation : nom de la pizza
+	 * @param prix : Prix de la pizza
+	 */
+
+	
+	public String toString() {
+		return code + " -> " + designation + "("+ prix + "€)";
+	}
+
+	public Pizza() {
 		
-		public static void displayAllPizza(Pizza[] pizzas) {
-			
-			for (Pizza pizza: pizzas) {
-				System.out.println(pizza.toString());
-				
-			}
-		}
-		public static Pizza[] addNewPizza(Pizza pizz ,Pizza[] pizzas) {
-				
-			Pizza[] newPizzas = new Pizza[pizzas.length+1];
- 			
-			for(int i=0;i<pizzas.length;i++) {
-				newPizzas[i] = pizzas[i];
-			}	
-			newPizzas[newPizzas.length-1] = pizz;
-			
-			return newPizzas;
-		}
-		public static Pizza[] deletePizza(String code,Pizza[] pizzas) {
-			
-			Pizza[] newPizzas = new Pizza[pizzas.length-1];
- 			int i =0;
-			
-			for (Pizza pizza : pizzas) {
-				
-				if(!pizza.getCode().equals(code)) {
-						newPizzas[i] = pizza; 
-						i++;
-					}
-			
-			}
-			return newPizzas;
-		}
-		public static Pizza[] updateNewPizza(String oldCode ,String code, Pizza[] pizzas,String name, Double price) {
-			for (Pizza pizza : pizzas) {
-				
-				if(pizza.getCode().equals(oldCode)) {
-					pizza.setCode(code);
-					pizza.setDesignation(name);
-					pizza.setPrice(price);
-				};
-			}
-			
-			return pizzas;
-		}
-		
+	}
+
+	/**
+	 * 
+	 * @return l'id de la pizza
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * 
+	 * @param id modifie l'id de la pizza
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * 
+	 * @return le code de la pizza
+	 */
+	public String getCode() {
+		return code;
+	}
+	/**
+	 * 
+	 * @param code de la pizza
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	/**
+	 * 
+	 * @return nom de la pizza
+	 */
+	public String getDesignation() {
+		return designation;
+	}
+	/**
+	 * 
+	 * @param nom de la pizza
+	 */
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	/**
+	 * 
+	 * @return le prix en float de la pizza en euro
+	 */
+	public double getPrix() {
+		return prix;
+	}
+	/**
+	 *
+	 * @param prix de la pizza en float
+	 */
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+
+	public Pizza(String code, String designation, double prix) {
+		this.id = count++;
+		this.code = code;
+		this.designation = designation;
+		this.prix = prix;			
+	}
+	
 }
